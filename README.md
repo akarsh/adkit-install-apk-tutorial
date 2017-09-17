bc8eef50ac421e4010af0757dc1b0add7a484472
akarsh <sakarshkumar@gmail.com>
2017-09-17 23:04:48 +0200

    Update Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 # adkit-install-apk-tutorial
 Tutorial for installing apk on android device using adbkit
+
+
+### Credits
+This project uses Open Source components. You can find the source code of their open source projects along with license information below. We acknowledge and are grateful to these developers for their contributions to open source.
+* Project: [adbkit](https://github.com/openstf/adbkit)
+  
+  Author: [9+ contributors](https://github.com/openstf/adbkit/graphs/contributors)
+
+  License: [Apache-2.0](https://github.com/openstf/adbkit/blob/master/LICENSE)
--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-// bluebird library https://www.npmjs.com/package/bluebird
-var Promise = require('bluebird')
-// adbkit library is a nodejs client for adb https://www.npmjs.com/package/adbkit
-var adb = require('adbkit')
-// request library to make http calls https://www.npmjs.com/package/request
-var request = require('request')
-// stream library https://www.npmjs.com/package/stream
-var Readable = require('stream').Readable
+var Promise = require('bluebird') // bluebird library https://www.npmjs.com/package/bluebird
+var adb = require('adbkit') // adbkit library is a nodejs client for adb https://www.npmjs.com/package/adbkit
+var request = require('request') // request library to make http calls https://www.npmjs.com/package/request
+var Readable = require('stream').Readable // stream library https://www.npmjs.com/package/stream
 var client = adb.createClient()
 
 client.listDevices()
